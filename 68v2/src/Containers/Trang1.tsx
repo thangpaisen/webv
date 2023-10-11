99cfe684932e643042a84826123be8dedfaaebdb
Nguyen Huu Thang <thangpaisen@gmail.com>
2023-10-12 00:40:19 +0700

    68v2

---
+++ 68v2/src/Containers/Trang1.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Trang1 = () => {
+  return (
+    <View>
+      <Text>Trang1</Text>
+    </View>
+  )
+}
+
+export default Trang1
+
+const styles = StyleSheet.create({})
